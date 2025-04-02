0a5be1bae39b4ae7fdf5192c02e8e328c71b7c7b
Mathias Dahlström <mathias.dahlstrom@gmail.com>
2025-04-02 15:28:03 +0200

    validation of client_secret was not right

--- example/variables.tf
+++ example/variables.tf
@@ -14,13 +14,6 @@ variable "client_secret" {
   type        = string
   default     = null
   description = "Client secret for service principal authentication. Only required when use_oidc is false or null."
-
-  validation {
-    condition = (
-      var.use_oidc == true || var.client_secret != null
-    )
-    error_message = "client_secret is required when use_oidc is false or null."
-  }
 }
 
 variable "use_oidc" {
