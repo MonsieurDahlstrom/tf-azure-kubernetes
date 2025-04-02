f1e278263b9208747dd1d4fd48932d6cdb5e269e
Mathias Dahlström <mathias.dahlstrom@gmail.com>
2025-04-02 15:25:27 +0200

    when use_oidc is set client_secret is not needed

--- example/variables.tf
+++ example/variables.tf
@@ -11,7 +11,16 @@ variable "client_id" {
 }
 
 variable "client_secret" {
-  type = string
+  type        = string
+  default     = null
+  description = "Client secret for service principal authentication. Only required when use_oidc is false or null."
+
+  validation {
+    condition = (
+      var.use_oidc == true || var.client_secret != null
+    )
+    error_message = "client_secret is required when use_oidc is false or null."
+  }
 }
 
 variable "use_oidc" {
