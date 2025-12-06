03e86ecf34fc9e6248719e9c7610799d34b527d3
Mathias Dahlström <mathias@monsieurdahlstrom.com>
2025-12-06 17:41:48 +0100

    azapi, azurerm providers moved to the latest versions, azapi moved to the latest api version as well

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_role_assignment" "aks_identity_operator" {
 }
 
 resource "azapi_resource" "aks" {
-  type      = "Microsoft.ContainerService/managedClusters@2024-10-02-preview"
+  type      = "Microsoft.ContainerService/managedClusters@2025-10-01"
   name      = var.cluster_name
   parent_id = data.azurerm_resource_group.aks.id
   location  = var.location
@@ -143,7 +143,7 @@ resource "azapi_resource" "aks" {
 }
 
 resource "azapi_resource_action" "aks_kubeconfig" {
-  type                   = "Microsoft.ContainerService/managedClusters@2024-10-02-preview"
+  type                   = "Microsoft.ContainerService/managedClusters@2025-10-01"
   resource_id            = azapi_resource.aks.id
   action                 = "listClusterAdminCredential"
   response_export_values = ["*"]
--- providers.tf
+++ providers.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 4.24.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 2.0"
+      version = ">= 2.3.0"
     }
   }
 } 
